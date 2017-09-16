314288620b2a6ec5b5e2c0ab45e796a4de96f78c
mo <mohamoud51@hotmail.com>
2017-09-16 18:38:37 -0400

    iterate method

--- UIConsole/Program.cs
+++ UIConsole/Program.cs
@@ -16,30 +16,58 @@ namespace UIConsole
             //this stops ef from going through its database initialization process
             //use for production, to not lose data, for development its okay
             Database.SetInitializer(new NullDatabaseInitializer<NinjaContext>());
-           // InsertNinja();
-           
 
+            // InsertNinja();
+            QIterateCollection();
         }
 
         private static void InsertNinja()
         {
-            //ninja has a dependeny on the clan and wont work unless a clan is created the clan Id = 1
+            //ninja has a dependeny on the clan and 
+            //wont work unless a clan is created the clan Id = 1:
             var ninja1 = new Ninja
-            {
-                Id = 1,
-                Name = "Mosh",
+            {                
+                Name = "rolo",
                 ServedInObiOne = false,
                 ClanId = 1
             };
-            //use using so you dont fuck around with the reasource
+            var ninja2 = new Ninja
+            {
+                Name = "jumand",
+                ServedInObiOne = true,
+                ClanId = 1
+            };
+
+            //use using so you dont fuck around with the reasource:
             using (var context = new NinjaContext())
             {
-                //Log the data from ef
+                //Log the data from ef:
                 context.Database.Log = Console.WriteLine;
 
-                //add and save methods for dbset:NinjaContext
-                context.Ninjas.Add(ninja1);
+                //Ninjacontext only adds one object at a time 
+                //unless using addRange method 
+                //takes in Ienumrable collection type:
+                context.Ninjas.AddRange(new List<Ninja>{ninja1,ninja2 });
+                
+                //add and save methods for dbset:NinjaContext:
+                //context.Ninjas.Add(ninja1); //----> regular single object add
+
+
                 context.SaveChanges();
+            }
+        }
+
+        private static void QIterateCollection()
+        {
+            using (var context = new NinjaContext())
+            {
+                var ninjas = context.Ninjas.ToList();
+                //var query = context.Ninjas;
+                //var someninjas = query.ToList();
+
+                //express querys using linq methods :----!!!!!!!!
+                // foreach loop leaves connection to db open
+                //(untill collection is iterated  through) if there is a large especially
             }
         }
     }
